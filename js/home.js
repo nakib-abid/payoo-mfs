0ab5c03cbba029bea53bd9e7dbbc3399605c80d6
nakib-abid <nakib.abid@gmail.com>
2024-09-22 14:07:46 +0600

    add an event handler to the add money button inside the form

--- js/home.js
+++ js/home.js
@@ -1,1 +1,24 @@
-console.log('home page visit')
+// add money to the account
+
+/**  step by step
+ * add event handler
+ * prevent page reload after form submit
+ * get money to be added to the account balance
+ * 
+ */
+
+
+// add an event handler to the add money button inside the form
+document.getElementById('btn-add-money')
+.addEventListener('click', function(event){
+    event.preventDefault(); //(prevent reloading browser on form submit)
+
+    // get money to be added to the account balance
+    const addMoneyInput = document.getElementById('input-add-money').value ;
+
+    // get the pin number provided
+    const pinNumberInput = document.getElementById('input-pin-number').value ;
+    console.log(addMoneyInput, pinNumberInput)
+    
+    
+})
